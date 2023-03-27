a7d73f2cd219efc2cb039072e7bd5aea29ddc60e
GavPri <gavinprior5@gmail.com>
2023-03-27 12:55:16 +0000

    add update surplus worksheet

--- run.py
+++ run.py
@@ -76,11 +76,19 @@ def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided.
     """
-    print('updating sales worksheet...\n')
+    print('Updating sales worksheet...\n')
     sales_worksheet = SHEET.worksheet('sales')
     sales_worksheet.append_row(data)
     print('Sales worksheet updated successfully\n')
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, adding a new row after calculations
+    """
+    print('Updating surplus worksheet\n')
+    surplus_worksheet = SHEET.worksheet('surplus')
+    surplus_worksheet.append_row(data)
+    print('Surplus worksheet updated successfully\n')
 
 def main():
     """
@@ -91,6 +99,7 @@ def main():
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
+    update_surplus_worksheet(data)
 
 print('Welcome To Love Sandwiches Automation\n')
 main()
