d117feca8dcd2b73ce1e5137d089f4b771a5bef6
GavPri <gavinprior5@gmail.com>
2023-03-27 13:23:47 +0000

    refractor functions to update worksheets

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figure input from the user
@@ -53,6 +54,16 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def update_worksheet(data,worksheet):
+    """
+    Recieves a list of integers and updates worksheets accordingly
+    """
+    print(f'Updating {worksheet} worksheet...\n')
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f'{worksheet} worksheet has been successfully updated.\n')
+
+
 def validate_data(values):
     """
     Inside the try,converts all string data into integers.
@@ -72,16 +83,6 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided.
-    """
-    print('Updating sales worksheet...\n')
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print('Sales worksheet updated successfully\n')
-
-def update_surplus_worksheet(data):
     """
     Update surplus worksheet, adding a new row after calculations
     """
@@ -90,16 +91,17 @@ def update_surplus_worksheet(data):
     surplus_worksheet.append_row(data)
     print('Surplus worksheet updated successfully\n')
 
+
 def main():
     """
     Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
-    update_surplus_worksheet(data)
+    update_worksheet(new_surplus_data, "surplus") 
 
 print('Welcome To Love Sandwiches Automation\n')
 main()
